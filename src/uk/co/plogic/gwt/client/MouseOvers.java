471e49867a2fa903dd23c928e716c1d0ad5628f3
Si Parker <si@plogic.co.uk>
2013-10-28 08:04:58 +0000

    added: non-map entry point and compile target

---
+++ src/uk/co/plogic/gwt/client/MouseOvers.java
@@ -0,0 +1,14 @@
+package uk.co.plogic.gwt.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.Window;
+
+public class MouseOvers implements EntryPoint {
+
+	@Override
+	public void onModuleLoad() {
+		Window.alert("hello world!");
+
+	}
+
+}
