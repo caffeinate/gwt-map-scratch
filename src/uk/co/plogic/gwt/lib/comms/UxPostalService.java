71ca8285deb4c83dc5afc612d920134f8837fb2a
Si Parker <si@plogic.co.uk>
2014-01-24 20:44:17 +0000

    refactored: DropBox isn't really an inner class

--- src/uk/co/plogic/gwt/lib/comms/UxPostalService.java
+++ src/uk/co/plogic/gwt/lib/comms/UxPostalService.java
@@ -1,10 +1,6 @@
 package uk.co.plogic.gwt.lib.comms;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
-
-import com.google.gwt.dev.util.collect.HashMap;
 
 
 public class UxPostalService {
@@ -17,42 +13,7 @@ public class UxPostalService {
 //		public HashMap<String, ArrayList<LetterBox<?>>> sectionLetterbox = new HashMap<String, ArrayList<LetterBox<?>>>();
 //	}
 	
-	/**
-	 * url + section => something (Generics Object)
-	 * @author si
-	 *
-	 */
-	class DropBox<E extends Object> {
-		
-		public HashMap<String, HashMap<String, ArrayList<E>>> boxes = new HashMap<String, HashMap<String, ArrayList<E>>>();
-		
-		public DropBox() {}
-		
-		public void add(String url, String section, E something) {
-			
-			if( ! boxes.containsKey(url) ) {
-				boxes.put(url, new HashMap<String, ArrayList<E>>());
-			}
 
-			HashMap<String, ArrayList<E>> s = boxes.get(url);
-			if( ! s.containsKey(section) ) {
-				s.put(section, new ArrayList<E>());
-			}
-			ArrayList<E> letter_boxes = s.get(section);
-			letter_boxes.add(something);
-		}
-		
-		/**
-		 * return all section names
-		 * @param url
-		 * @return 
-		 */
-		public Set<String> get(String url) {
-			return boxes.get(url).keySet();
-		}
-
-	}
-	
 	public class RegisteredLetterBox {
 
 		private String envelopeSection;
