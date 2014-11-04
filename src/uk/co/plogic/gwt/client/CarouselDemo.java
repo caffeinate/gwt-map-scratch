8a91c4a56028d172a83deb984e75f3cd462cd50b
Si Parker <si@plogic.co.uk>
2014-11-04 17:46:11 +0000

    updated: carousel - now works nicely and can add a fixed header. Doesn't correctly position itself, it should replace the element it takes it's data from.

--- src/uk/co/plogic/gwt/client/CarouselDemo.java
+++ src/uk/co/plogic/gwt/client/CarouselDemo.java
@@ -1,29 +1,40 @@
 package uk.co.plogic.gwt.client;
 
+import java.util.ArrayList;
+import java.util.logging.Logger;
+
+import uk.co.plogic.gwt.lib.dom.DomElementByClassNameFinder;
+import uk.co.plogic.gwt.lib.dom.DomParser;
 import uk.co.plogic.gwt.lib.widget.Carousel;
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.dom.client.Element;
 import com.google.gwt.user.client.ui.RootPanel;
 
 public class CarouselDemo implements EntryPoint {
 
+	final Logger logger = Logger.getLogger("Carousel");
 
 	@Override
 	public void onModuleLoad() {
 
 		RootPanel r = RootPanel.get("container");
-		Carousel c = new Carousel();
-		r.add(c);
 
-		for( String colour : new String [] {"red", "blue", "green"}) {
-			FlowPanel tile = new FlowPanel();
-			tile.setPixelSize(100, 100);
-			tile.setStyleName(colour);
-			c.addWidget(tile);
-		}
+		DomParser domParser = new DomParser();
+		final ArrayList<Element> carouselElements = new ArrayList<Element>();
+	    domParser.addHandler(new DomElementByClassNameFinder("carousel") {
+	        @Override
+	        public void onDomElementFound(Element element, String id) {
+	        	carouselElements.add(element);
+	        }
+	    });
+	    domParser.parseDom();
 
-		
+	    for(Element e : carouselElements) {
+			Carousel c = new Carousel();
+			r.add(c);
+			c.pagesFromDomElement(e);
+	    }
+
 	}
-	
 }
--- src/uk/co/plogic/gwt/lib/widget/Carousel.java
+++ src/uk/co/plogic/gwt/lib/widget/Carousel.java
@@ -3,13 +3,19 @@ package uk.co.plogic.gwt.lib.widget;
 import java.util.ArrayList;
 import java.util.logging.Logger;
 
+import uk.co.plogic.gwt.lib.dom.DomElementByClassNameFinder;
+import uk.co.plogic.gwt.lib.dom.DomParser;
+
 import com.google.gwt.animation.client.Animation;
+import com.google.gwt.dom.client.Element;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.ui.AbsolutePanel;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.FocusPanel;
+import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.Widget;
 
 /**
@@ -22,13 +28,17 @@ import com.google.gwt.user.client.ui.Widget;
 public class Carousel extends Composite {
 
 	final Logger logger = Logger.getLogger("Carousel");
-	final AbsolutePanel viewport = new AbsolutePanel();
-	int width = 100;
-	int height = 100;
+	FocusPanel holdingPanel = new FocusPanel();
+	AbsolutePanel viewport = new AbsolutePanel();
+	int width = 0;
+	int height = 0;
+	int headerOffset = 0; // if there is a fixed header section
 	int currentWidget = -1;
 	int widgetCount = 0;
 	ArrayList<Widget> widgets = new ArrayList<Widget>();
 	static int animationDuration = 350;
+	Element parentElement;
+	ArrayList<Element> pages = new ArrayList<Element>();
 	
 	class AnimateViewpoint extends Animation {
 
@@ -45,18 +55,79 @@ public class Carousel extends Composite {
 		@Override
 		protected void onUpdate(double progress) {
 			int currentPos = (int) (w1_start + (width * progress * direction));
-			viewport.setWidgetPosition(w1, currentPos, 0);
+			viewport.setWidgetPosition(w1, currentPos, headerOffset);
 			currentPos = (int) (w2_start + (width * progress * direction));
-			viewport.setWidgetPosition(w2, currentPos, 0);
+			viewport.setWidgetPosition(w2, currentPos, headerOffset);
 		}
 
 	}
 
 	public Carousel() {
 
-		FlowPanel holdingPanel = new FlowPanel();
-		viewport.setPixelSize(width, height);
-		viewport.setStyleName("carousel_viewpoint");
+	    holdingPanel.add(viewport);
+	    holdingPanel.addClickHandler(new ClickHandler() {
+			@Override
+			public void onClick(ClickEvent event) {
+				moveTo(1);
+			}
+	    });
+
+	    //setupControls();
+		initWidget(holdingPanel);
+	}
+
+	public void pagesFromDomElement(Element e) {
+
+		parentElement = e;
+
+		DomParser domParser = new DomParser();
+	    domParser.addHandler(new DomElementByClassNameFinder("carousel_page") {
+	        @Override
+	        public void onDomElementFound(Element element, String id) {
+	        	pages.add(element);
+	        }
+	    });
+	    domParser.parseDom(parentElement);
+
+	    width = parentElement.getOffsetWidth();
+	    height = parentElement.getOffsetHeight();
+	    viewport.setPixelSize(width, height);
+
+    	// remove pages from parent and set them up
+    	ArrayList<FlowPanel> pagePanels = new ArrayList<FlowPanel>();
+	    for(Element p: pages) {
+	    	FlowPanel fp = new FlowPanel();
+	    	Element fpe = fp.getElement();
+	    	// TODO - copy any other attributes?
+	    	fpe.setClassName(p.getClassName());
+	    	fpe.setId(p.getId());
+	    	fpe.setInnerHTML(p.getInnerHTML());
+	    	p.removeFromParent();
+	    	pagePanels.add(fp);
+	    }
+
+	    Element vpe = viewport.getElement();
+    	// TODO - copy any other attributes?
+    	vpe.setClassName(parentElement.getClassName());
+    	vpe.setId(parentElement.getId());
+
+    	String headerCopy = parentElement.getInnerHTML().trim();
+    	if( headerCopy.length() > 0 ) {
+    		HTML fixedHeader = new HTML(parentElement.getInnerHTML());
+    		viewport.add(fixedHeader, 0, 0);
+    		// TODO set header's height on page ready or page resize
+    		headerOffset = fixedHeader.getOffsetHeight();
+    	}
+    	parentElement.removeFromParent();	    
+
+    	int contentsHeight = height-headerOffset;
+	    for(FlowPanel fp : pagePanels) {
+	    	fp.setHeight(""+contentsHeight+"px");
+	    	addWidget(fp);
+	    }
+	}
+
+	private void setupControls() {
 
 	    Button previous = new Button("Previous");
 	    previous.addClickHandler(new ClickHandler() {
@@ -72,11 +143,9 @@ public class Carousel extends Composite {
 				moveTo(1);
 			}
 	    });
-
-	    holdingPanel.add(viewport);
 	    holdingPanel.add(previous);
 	    holdingPanel.add(next);
-		initWidget(holdingPanel);
+
 	}
 
 	public void moveTo(int direction) {
@@ -87,7 +156,7 @@ public class Carousel extends Composite {
 		
 		// position widgetToShow to one side of viewpoint
 		Widget widgetToShow = widgets.get(widgetToShowIndex);
-		viewport.setWidgetPosition(widgetToShow, width*direction, 0);
+		viewport.setWidgetPosition(widgetToShow, width*direction, headerOffset);
 		
 		Widget current = widgets.get(currentWidget);
 		AnimateViewpoint av = new AnimateViewpoint( direction*-1, widgetToShow, current);
@@ -99,11 +168,13 @@ public class Carousel extends Composite {
 		widgets.add(w);
 		if( currentWidget == -1 ) {
 			currentWidget = 0;
-			viewport.add(w, 0, 0);
+			viewport.add(w, 0, headerOffset);
 		} else {
 			// put it somewhere out of sight
 			viewport.add(w, 0, height+10);
 		}
 		widgetCount = widgets.size();
 	}
+
+
 }
