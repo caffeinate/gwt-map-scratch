b8b40fa095f909872358bc9c504b83e592301eea
Si Parker <si@plogic.co.uk>
2015-02-03 14:45:40 +0000

    tidied: and re-fitted couple of charts for gwt-charts - not tested or checked

--- src/uk/co/plogic/gwt/lib/widget/dataVisualisation/BarChartVisualisation.java
+++ src/uk/co/plogic/gwt/lib/widget/dataVisualisation/BarChartVisualisation.java
@@ -1,15 +1,15 @@
 package uk.co.plogic.gwt.lib.widget.dataVisualisation;
 
-import com.google.gwt.core.client.JavaScriptObject;
-import com.google.gwt.core.client.JsArrayString;
 import com.google.gwt.dom.client.Element;
 import com.google.gwt.event.shared.HandlerManager;
 import com.google.gwt.user.client.ui.Widget;
-import com.google.gwt.visualization.client.ChartArea;
-import com.google.gwt.visualization.client.visualizations.corechart.BarChart;
-import com.google.gwt.visualization.client.visualizations.corechart.HorizontalAxisOptions;
-import com.google.gwt.visualization.client.visualizations.corechart.Options;
-import com.google.maps.gwt.client.ArrayHelper;
+import com.googlecode.gwt.charts.client.ChartPackage;
+import com.googlecode.gwt.charts.client.corechart.BarChart;
+import com.googlecode.gwt.charts.client.corechart.BarChartOptions;
+import com.googlecode.gwt.charts.client.options.HAxis;
+import com.googlecode.gwt.charts.client.options.Legend;
+import com.googlecode.gwt.charts.client.options.LegendPosition;
+import com.googlecode.gwt.charts.client.options.TextPosition;
 
 
 public class BarChartVisualisation extends ChartVisualisation {
@@ -20,58 +20,70 @@ public class BarChartVisualisation extends ChartVisualisation {
 	public BarChartVisualisation(HandlerManager eventBus, final Element e) {
 
 		//super(eventBus, e, ColumnChart.PACKAGE);
-		super(eventBus, e, BarChart.PACKAGE);
+	    super(eventBus, e, ChartPackage.CORECHART);
 		setupEventHandling();
 	}
 
     @Override
-    public Options createOptions() {
-        Options options = Options.create();
+    public BarChartOptions createOptions() {
+        BarChartOptions options = BarChartOptions.create();
 
         int pWidth = responsiveSizing.getWidth();
         options.setWidth(pWidth);
         options.setHeight(responsiveSizing.getHeight());
         //options.set("hAxis.viewWindow.max", 100.0);
-        options.set("vAxis", barChartSpecialOptions());
-        options.setLegend("top");
-        ChartArea chartArea = ChartArea.create();
-        chartArea.setLeft((int) (pWidth*0.33));
+        //options.set("vAxis", barChartSpecialOptions());
+        options.setLegend(Legend.create(LegendPosition.NONE));
+        //ChartArea chartArea = ChartArea.create();
+        //chartArea.setLeft((int) (pWidth*0.33));
         //chartArea.setHeight("85%");
         //chartArea.setWidth("65%");
         //chartArea.setWidth(100);
-        options.setChartArea(chartArea);
+//        options.setChartArea(chartArea);
 
-        HorizontalAxisOptions hOptions = HorizontalAxisOptions.create();
-        hOptions.setMinValue(0.0);
-        hOptions.setMaxValue(20.0);
-        hOptions.setShowTextEvery(1);
-        hOptions.set("format", "#'%'");
-        options.setHAxisOptions(hOptions);
+//        HorizontalAxisOptions hOptions = HorizontalAxisOptions.create();
+//        hOptions.setMinValue(0.0);
+//        hOptions.setMaxValue(20.0);
+//        hOptions.setShowTextEvery(1);
+//        hOptions.set("format", "#'%'");
+//        options.setHAxisOptions(hOptions);
+
+        HAxis hAxis = HAxis.create();
+        hAxis.setTextPosition(TextPosition.NONE);
+        options.setHAxis(hAxis);
 
         //options.setColors("blue");
         //options.setColors("green");
 
-        String [] c = {"red", "gray"};
-        JsArrayString x = ArrayHelper.toJsArrayString(c);
-        options.setColors(x);
+        options.setColors("red");
+
+//        String [] c = {"red", "gray"};
+//        JsArrayString x = ArrayHelper.toJsArrayString(c);
+//        options.setColors(x);
 
         return options;
     }
 
     @Override
     protected Widget redraw() {
+
+        if( chartDataTable == null)
+            return (Widget) null;
+
         if( chart == null ) {
             //chart = new ColumnChart(dataTable, options);
-            chart = new BarChart(chartDataTable, createOptions());
+            chart = new BarChart();
+
+            chart.draw(chartDataTable, createOptions());
             return (Widget) chart;
         } else {
-            chart.draw(chartDataTable, createOptions());
+            chart.redraw();
         }
-        return null;
+        return (Widget) null;
     }
 
-    public static native JavaScriptObject barChartSpecialOptions() /*-{
-        return { textStyle : {fontSize: 10} };
-    }-*/;
+//    public static native JavaScriptObject barChartSpecialOptions() /*-{
+//        return { textStyle : {fontSize: 10} };
+//    }-*/;
 
 }
--- src/uk/co/plogic/gwt/lib/widget/dataVisualisation/PieChartVisualisation.java
+++ src/uk/co/plogic/gwt/lib/widget/dataVisualisation/PieChartVisualisation.java
@@ -1,12 +1,11 @@
 package uk.co.plogic.gwt.lib.widget.dataVisualisation;
 
-import com.google.gwt.core.client.JavaScriptObject;
 import com.google.gwt.dom.client.Element;
 import com.google.gwt.event.shared.HandlerManager;
 import com.google.gwt.user.client.ui.Widget;
-import com.google.gwt.visualization.client.visualizations.corechart.Options;
-import com.google.gwt.visualization.client.visualizations.corechart.PieChart;
-import com.google.gwt.visualization.client.visualizations.corechart.PieChart.PieOptions;
+import com.googlecode.gwt.charts.client.ChartPackage;
+import com.googlecode.gwt.charts.client.corechart.PieChart;
+import com.googlecode.gwt.charts.client.corechart.PieChartOptions;
 
 
 public class PieChartVisualisation extends ChartVisualisation {
@@ -14,15 +13,19 @@ public class PieChartVisualisation extends ChartVisualisation {
 	PieChart pie;
 
 	public PieChartVisualisation(HandlerManager eventBus, final Element e) {
-		super(eventBus, e, PieChart.PACKAGE);
+	    super(eventBus, e, ChartPackage.CORECHART);
 		setupEventHandling();
 	}
 
     @Override
     protected Widget redraw() {
+        if( chartDataTable == null)
+            return (Widget) null;
+
         if( pie == null ) {
             //chart = new ColumnChart(dataTable, options);
-            pie = new PieChart(chartDataTable, createOptions());
+            pie = new PieChart();
+            pie.draw(chartDataTable, createOptions());
             return (Widget) pie;
         } else {
             pie.draw(chartDataTable, createOptions());
@@ -30,14 +33,14 @@ public class PieChartVisualisation extends ChartVisualisation {
         return null;
     }
 
-    public Options createOptions() {
+    public PieChartOptions createOptions() {
 
-        PieOptions options = PieOptions.create();
+        PieChartOptions options = PieChartOptions.create();
 
 	    options.setWidth(responsiveSizing.getWidth());
 	    options.setHeight(responsiveSizing.getHeight());
-	    options.set3D(true);
-	    options.set("legend", pieChartSpecialOptions());
+	    //options.set3D(true);
+	    //options.set("legend", pieChartSpecialOptions());
 	    if( getTitle() != null )
 	    	options.setTitle(getTitle());
 	    //options.setLegend(LegendPosition.BOTTOM);
@@ -52,9 +55,9 @@ public class PieChartVisualisation extends ChartVisualisation {
 	    return options;
 	}
 
-    public static native JavaScriptObject pieChartSpecialOptions() /*-{
-		return {position: 'top', maxLines: 4};
-	}-*/;
+//    public static native JavaScriptObject pieChartSpecialOptions() /*-{
+//		return {position: 'top', maxLines: 4};
+//	}-*/;
     //return {position: 'top', textStyle: {color: 'blue', fontSize: 16}};
 
 }
