c128b705c825099c7c1b7d5db9ecb485f6340792
Si Parker <si@plogic.co.uk>
2015-02-20 16:21:11 +0000

    added: docs to GazetteerJso

--- src/uk/co/plogic/gwt/lib/jso/GazetteerJso.java
+++ src/uk/co/plogic/gwt/lib/jso/GazetteerJso.java
@@ -3,7 +3,7 @@ package uk.co.plogic.gwt.lib.jso;
 import com.google.gwt.core.client.JavaScriptObject;
 
 final public class GazetteerJso extends JavaScriptObject {
-	
+
 	protected GazetteerJso() {}
 
 	public native String getUrl()/*-{
@@ -23,9 +23,10 @@ final public class GazetteerJso extends JavaScriptObject {
 	public native String getInputElement() /*-{
 		return this.hasOwnProperty("input_element") ? this.input_element : null;
 	}-*/;
-	
+
 	/**
-	 * where any output should be written
+	 * where in the DOM to place the drop down style input. This is optional. If not
+	 * provided then just the map-control type gazetteer-control will be used.
 	 * @return
 	 */
 	public native String getTargetElement() /*-{
